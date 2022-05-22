3ddc86a36a8ba9d9c0cb8955dee03b8e6d34b4a2
saanviamrutha <102648734+saanviamrutha@users.noreply.github.com>
2022-05-22 12:10:11 +0530

    Add files via upload

---
+++ Codes/Assignment5.py
@@ -0,0 +1,13 @@
+#Given
+X_0 = 0
+X_1 = 1
+P_X_0 = 0.3
+P_X_1 = 0.7
+
+#Finding E(X)
+E_X = X_0*P_X_0+X_1*P_X_1 #formula
+print("E(X) =",E_X)
+
+#Finding Var(X)
+Var_X = X_0**2*P_X_0 + X_1**2*P_X_1 - E_X**2 #formula
+print("Var(X) =",Var_X)
